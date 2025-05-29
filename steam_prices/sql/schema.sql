ff01cd18254b3268f139ab9daf76e6cbb0b798dc
Tobias <tobiasalsgaard@gmail.com>
2025-05-29 17:24:56 +0200

    removed empty line

--- steam_prices/sql/schema.sql
+++ steam_prices/sql/schema.sql
@@ -25,7 +25,6 @@ create TABLE steam_stats.dates(
     oldest_date TIMESTAMP,
     FOREIGN KEY(steam_game_id) REFERENCES steam_stats.game(id),
     FOREIGN KEY(newest_date, oldest_date) REFERENCES steam_stats.update_release(newest_date, oldest_date)
-
 );
 
 CREATE TABLE steam_stats.costs(
