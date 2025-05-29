acdcb5d40b2bda4dbf6980fd1466a264bd1b3684
Tobias <tobiasalsgaard@gmail.com>
2025-05-29 18:25:43 +0200

    quick update to .sh

--- steam_prices/bash/setup_new_db.sh
+++ steam_prices/bash/setup_new_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 clear
-psql --dbname=steam_db --username=postgres -f ../sql/create_new_db.sql
+psql --dbname=postgres --username=postgres -f ../sql/create_new_db.sql
---
+++ steam_prices/bash/setup_new_db_windows.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+clear
+psql.exe --dbname=postgres --username=postgres -f ../sql/create_new_db.sql
