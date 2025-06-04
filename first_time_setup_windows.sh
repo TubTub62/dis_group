9176949cbe14b835032730765f89c80046d0ca6c
TubTub62 <32617994+TubTub62@users.noreply.github.com>
2025-06-04 21:39:35 +0200

    Create first_time_setup_windows.sh
    windows specific first time file

---
+++ first_time_setup_windows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 -m venv venv # or just python for some
+source venv/script/activate # venv/script/activate for windows(might need to be capital S in Script)
+
+cd steam_prices
+pip install -r requirements.txt
+
+cd bash
+
+bash create_new_db_server.sh
+bash run.sh
